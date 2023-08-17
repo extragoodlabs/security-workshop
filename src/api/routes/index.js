f08215d579d7cf7a3f54a060f7d6ec2f3fbeec67
William Huba <william@jumpwire.io>
2023-08-17 14:41:24 -0400

    Set new default error message for / route

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.status(404).json({error: "not found"});
+    res.status(404).json({error: "unknown"});
 });
 
 module.exports = router;
