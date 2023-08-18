e71af391b596c33ef32bffeb1cecb30d674b9b23
William Huba <william@jumpwire.io>
2023-08-17 21:51:57 -0400

    Use a JS file for configuring the API node app

---
+++ src/api/config/default.js
@@ -0,0 +1,10 @@
+module.exports = {
+    database: {
+        dialect: "postgres",
+        host: "localhost",
+        port: 5432,
+        database: "bank",
+        username: "postgres",
+        password: "postgres"
+    }
+}
